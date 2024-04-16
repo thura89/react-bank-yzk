fab414b63c62beff49d827b9dc8a47690f35296e
Thura Freelance <mozet89@gmail.com>
2024-04-16 17:21:33 +0800

    create clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,22 @@
-const Clients = () => {
-  return <div>Clients</div>;
-};
+import { clients } from "../constants";
+import styles from "../style";
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`${styles.flexCenter} flex-1 min-w-[120px] sm:min-w-[192px] m-5`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="w-[100px] sm:w-[192px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
